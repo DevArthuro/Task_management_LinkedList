857620ffc03febf00d8573b1c20bceed9dd0a122
carlos <caorrego38@soy.sena.edu.co>
2023-10-25 16:11:34 -0500

    style(components templates) add the form to components to template

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 from src.routes.task_management.task import app_route_task
 from uvicorn import run
+from environ import Env
+from fastapi.middleware.cors import CORSMiddleware
+
+env = Env()
+Env.read_env()
+
+
 
 app = FastAPI()
-
+app.middleware(CORSMiddleware(
+    app,
+    allow_credentials=["*"],
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+))
 
 app.include_router(app_route_task, prefix="/manage-task")
 
 if __name__ == "__main__":
-    run(app)
+    run(app, host = env("HOST_SERVER"), port = int(env("PORT_SERVER")))
